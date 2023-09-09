2c7ed31958265654f49d9f4992d1ae3ea041f8bc
Highly-Unstable-Hadron <102580192+Highly-Unstable-Hadron@users.noreply.github.com>
2023-09-09 10:44:00 +0530

    Added "onkeypress" search suggestions

--- soundache-backend/main.py
+++ soundache-backend/main.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, session, url_for, send_from_directory, send_file
 import sqlite3
+import difflib
 
 SOUNDACHE = "Soundache"
 
@@ -12,10 +13,16 @@ def main_page():
     fetched = cursor.execute("SELECT * FROM recommended_songs")
     return render_template("index.html", songs=fetched.fetchall())
 
-@app.route("/search", methods=["GET", "POST"])
+@app.route("/search")
 def search():
-    print(request.form['searchbar'])
-    return "You searched: " + request.form['searchbar']
+    query = request.args.get("searchbar")
+    elements = ['Actinium', 'Aluminum', 'Americium', 'Antimony', 'Argon', 'Arsenic', 'Astatine', 'Barium', 'Berkelium', 'Beryllium', 'Bismuth', 'Bohrium', 'Boron', 'Bromine', 'Cadmium', 'Calcium', 'Californium', 'Carbon', 'Cerium', 'Cesium', 'Chlorine', 'Chromium', 'Cobalt', 'Copper', 'Curium', 'Darmstadtium', 'Dubnium', 'Dysprosium', 'Einsteinium', 'Erbium', 'Europium', 'Fermium', 'Fluorine', 'Francium', 'Gadolinium', 'Gallium', 'Germanium', 'Gold', 'Hafnium', 'Hassium', 'Helium', 'Holmium', 'Hydrogen','Indium','Iodine','Iridium','Iron','Krypton','Lanthanum','Lawrencium','Lead','Lithium','Livermorium','Lutetium','Magnesium','Manganese','Meitnerium','Mendelevium','Mercury','Molybdenum','Moscovium','Neodymium','Neon','Neptunium','Nickel','Nihonium','Niobium','Nitrogen','Nobelium','Oganesson','Osmium','Oxygen','Palladium','Phosphorus','Platinum','Plutonium','Polonium','Potassium','Praseodymium','Promethium','Protactinium','Radium','Radon','Rhenium', 'Rhodium'
+                ]
+    lst = difflib.get_close_matches(query.capitalize(), elements, 16, min(0.8, 0.1*len(query)))
+
+    if request.headers.get("API", False):
+        return {"results": lst}
+    return f"You searched {query}\n{lst}"
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
